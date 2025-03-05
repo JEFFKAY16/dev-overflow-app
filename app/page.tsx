59681ebe1e01d11bd6525b3397e6e3dc31b46d73
JEFFKAY16 <jeffkayombo16@gmail.com>
2025-03-05 12:52:52 +0200

    Add meta data and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-white p-4 text-black">
-      <p className="text-teal-800">A better version of stack overflow</p>
-    </div>
-  );
+  return <div className="font-space-grotesk">Hello world</div>;
 }
